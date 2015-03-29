212957208b6cee1e7e312e4d171021e3e0225936
givdwiel <givdwiel.vandewiele@ugent.be>
2015-03-29 18:22:30 +0200

    BaselineScheduleRecord

--- PMConverter/src/object/baselineschedule.py
+++ PMConverter/src/object/baselineschedule.py
@@ -1,18 +1,31 @@
 __author__ = 'PM Group 8'
 from datetime import datetime, timedelta
 
-class BaselineScheduleRecord(object):
-    "Contains a baseline start, baseline end and duration of an activity"
-    # instance variables:
-    # _baselineStart: datetime indicating the scheduled start
-    # _baselineEnd: datetime indicating the scheduled end
-    # _duration: timedelta = time between _baselineEnd and _baselineStart
 
-    def __init__(self):
-        pass
+class BaselineScheduleRecord(object):
+    """
+    Record used in the baseline schedule. Contains the start, end (and calculated from this, the duration),
+    the fixed costs, cost per hour and variable costs for every activity
+    :var start: String (date)
+    :var end: String (date)
+    :var fixed_cost: float
+    :var cost_hourly: float
+    :var var_cost: float
+    """
+
+    def __init__(self, start, end, fixed_cost, cost_hourly, var_cost):
+        self.start = start
+        self.end = end
+        self.fixed_cost = fixed_cost
+        self.cost_hourly = cost_hourly
+        self.var_cost = var_cost
 
     def get_duration_string(self):
-        "Return the duration in desired string format"
+        """
+        First we calculate the difference between the end and the start, then this is converted to a date format.
+
+        :return difference between end and start in a date format
+        """
 
         if self._duration.seconds > 0:
             return "{0}d {1}h".format(self._duration.days, self._duration.seconds//3600)
