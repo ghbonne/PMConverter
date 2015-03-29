26739be88ae581d47b14e0cb377370270f9be5a3
givdwiel <givdwiel.vandewiele@ugent.be>
2015-03-29 18:13:22 +0200

    moved some variables to activity level

--- PMConverter/src/object/projectobject.py
+++ PMConverter/src/object/projectobject.py
@@ -9,18 +9,13 @@ class ProjectObject(object):
     and project control
 
     :var name: String
-    :var baseline_schedule: BaselineScheduleRecord
-    :var baseline_costs: dictionary containing "Fixed Cost", "Cost/Hour", "Variable Cost" and "Total Cost" if nonzero
     :var activities: list of Activity
     :var tracking_periods: list of TrackingPeriod
     :var resources: list of Resource
     """
 
-    def __init__(self, name="", baseline_schedule=BaselineScheduleRecord(), baseline_costs={}, activities=[],
-                 tracking_periods=[], resources=[]):
+    def __init__(self, name="", activities=[], tracking_periods=[], resources=[]):
         self.name = name
-        self._baselineSchedule = baseline_schedule
-        self._baselineCosts = baseline_costs
         self._activityList = activities
         self._trackingPeriodsList = tracking_periods
         self._resourcesList = resources
