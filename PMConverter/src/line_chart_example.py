02a6e8107393805ed2add44baeec6b77f7ad79a9
ehoogsto <evelinekeh@gmail.com>
2015-03-30 11:39:01 +0200

    made output file path independent of OS

--- PMConverter/src/line_chart_example.py
+++ PMConverter/src/line_chart_example.py
@@ -4,7 +4,7 @@ import xlsxwriter
 import os
 from visual.linechart import LineChart
 
-file = 'output/chart_test.xlsx'
+file = 'output'+os.sep+'chart_test.xlsx'
 
 workbook = xlsxwriter.Workbook(file)
 worksheet = workbook.add_worksheet()
