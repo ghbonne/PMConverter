da1a49e27febf0ab515d4d2f685c8739c23fc63d
ehoogsto <evelinekeh@gmail.com>
2015-04-24 07:59:28 +0200

    import issues => moved available visualisations

--- PMConverter/src/processor/processor.py
+++ PMConverter/src/processor/processor.py
@@ -4,12 +4,28 @@ from convert.XLSXparser import XLSXParser
 from convert.XMLparser import XMLParser
 from visual.charts.linechart import LineChart
 from visual.charts.piechart import PieChart
+from visual.baselineshedule import BaselineSchedule
+from visual.actualcost import ActualCost
+from visual.resourcedistribution import ResourceDistribution
+from visual.riskanalysis import RiskAnalysis
+from visual.actualduration import ActualDuration
+from visual.cpi import CPI
+from visual.cost_value_metrics import CostValueMetrics
+from visual.sv_t import SvT
+from visual.spi_t import SpiT
+from visual.spi_t_p_factor import SpiTvsPfactor
+from visual.performance import Performance
+from visual.budget import CV
 
 
 class Processor(object):
 
     def __init__(self):
-        self.visualizations = []
+        self.visualizations = [BaselineSchedule(), "--",
+           ResourceDistribution(), "--",
+           RiskAnalysis(), "--",
+           ActualDuration(), ActualCost(), "--",
+           CostValueMetrics(), Performance(), SpiTvsPfactor(), SvT(), CV(), CPI(), SpiT()]
         self.file_parsers = []
 
     def convert(self, parser_from, parser_to, file_path_from, file_path_to):
--- PMConverter/src/visual/visualization.py
+++ PMConverter/src/visual/visualization.py
@@ -1,25 +1,6 @@
 __author__ = 'Eveline'
-from visual.actualcost import ActualCost
-from visual.actualduration import ActualDuration
-from visual.baselineshedule import BaselineSchedule
-from visual.budget import CV
-from visual.cost_value_metrics import CostValueMetrics
-from visual.cpi import CPI
-from visual.performance import Performance
-from visual.resourcedistribution import ResourceDistribution
-from visual.riskanalysis import RiskAnalysis
-from visual.spi_t import SpiT
-from visual.sv_t import SvT
-from visual.spi_t_p_factor import SpiTvsPfactor
-
 
 class Visualization(object):
 
-    all = [BaselineSchedule(), "--",
-           ResourceDistribution(), "--",
-           RiskAnalysis(), "--",
-           ActualDuration(), ActualCost(), "--",
-           CostValueMetrics(), Performance(), SpiTvsPfactor(), SvT(), CV(), CPI(), SpiT()]
-
-    def draw(self, workbook):
+    def draw(self, workbook, worksheet):
         raise NotImplementedError("This method is not implemented!")
