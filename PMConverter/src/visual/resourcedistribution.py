c8612f7ad05112d4e800e1e09d6cad11ff10deb1
ehoogsto <evelinekeh@gmail.com>
2015-04-24 08:00:28 +0200

    implemented ressourcedistribution

--- PMConverter/src/visual/resourcedistribution.py
+++ PMConverter/src/visual/resourcedistribution.py
@@ -1,6 +1,7 @@
 __author__ = 'Eveline'
 from visual.visualization import Visualization
 from visual.enums import DataType
+from visual.charts.piechart import PieChart
 
 
 class ResourceDistribution(Visualization):
@@ -15,3 +16,21 @@ class ResourceDistribution(Visualization):
         self.title = "Resource costs"
         self.description = ""
         self.parameters = {'data_type': [DataType.ABSOLUTE, DataType.RELATIVE]}
+
+    def draw(self, workbook, worksheet):
+        if not self.data_type:
+            raise Exception("Please first set var data_type")
+
+        data_series2 = [
+            ['Resources', 2, 1, 13, 1],
+            ['Resources', 2, 7, 13, 7]
+        ]
+
+        if self.data_type == DataType.RELATIVE:
+            relative = True
+        else:
+            relative = False
+
+        chart2 = PieChart('Resources', data_series2, relative)
+
+        chart2.draw(workbook, worksheet, 'I1')
