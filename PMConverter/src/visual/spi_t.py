bff0c5853decb517ed5f3448aa77e20dcb655878
ehoogsto <evelinekeh@gmail.com>
2015-04-25 23:18:06 +0200

    adding threshold - calculate values

--- PMConverter/src/visual/spi_t.py
+++ PMConverter/src/visual/spi_t.py
@@ -17,6 +17,7 @@ class SpiT(Visualization):
         self.parameters = {"threshold": True,
                            "x-axis": [XAxis.TRACKING_PERIOD, XAxis.DATE]}
         self.x_axis = None
+        self.threshold = None
 
     def draw(self, workbook, worksheet, project_object):
         #todo: threshold
@@ -27,6 +28,9 @@ class SpiT(Visualization):
 
         # number tracking periods
         tp_size = len(project_object.tracking_periods)
+
+        if self.threshold and self.threshold != (0, 0):
+            self.calculate_threshold(workbook, worksheet, tp_size)
 
         # values for x_axis
         if self.x_axis == XAxis.TRACKING_PERIOD:
@@ -46,3 +50,22 @@ class SpiT(Visualization):
 
         size = {'width': 750, 'height': 500}
         chart.draw(workbook, chartsheet, 'A1', None, size)
+
+    def calculate_threshold(self, workbook, worksheet, tp_size):
+        header = workbook.add_format({'bold': True, 'bg_color': '#316AC5', 'font_color': 'white', 'text_wrap': True,
+                                      'border': 1, 'font_size': 8})
+        calculation = workbook.add_format({'bg_color': '#FFF2CC', 'text_wrap': True, 'border': 1, 'font_size': 8})
+
+        worksheet.write('AD4', 'SPI(t) threshold', header)
+
+        start = 2
+        if self.threshold[0] == self.threshold[1]:
+            for i in range(0, tp_size):
+                worksheet.write(start + i, 29, self.threshold[0], calculation)
+        else:
+            if self.threshold[0] > self.threshold[1]:
+                value = (self.threshold[0] - self.threshold[1])/tp_size
+            else:
+                value = (self.threshold[1] - self.threshold[0])/tp_size
+            for i in range(0, tp_size):
+                worksheet.write(start + i, 29, value, calculation)
