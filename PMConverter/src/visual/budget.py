65faf82e352953b0f4f3d5a6b55314acea6ed49e
ehoogsto <evelinekeh@gmail.com>
2015-04-25 16:09:20 +0200

    implemented CV curve

--- PMConverter/src/visual/budget.py
+++ PMConverter/src/visual/budget.py
@@ -1,6 +1,7 @@
 __author__ = 'Eveline'
 from visual.visualization import Visualization
-from visual.enums import DataType
+from visual.enums import XAxis
+from visual.charts.linechart import LineChart
 
 
 class CV(Visualization):
@@ -11,4 +12,33 @@ class CV(Visualization):
     def __init__(self):
         self.title = "CV"
         self.description = ""
-        self.parameters = {}
+        self.parameters = {"x_axis": [XAxis.TRACKING_PERIOD, XAxis.DATE]}
+        self.x_axis = None
+
+    def draw(self, workbook, worksheet, project_object): #todo probleem met percenten
+        if not self.x_axis:
+            raise Exception("Please first set var x_axis")
+
+        chartsheet = workbook.add_worksheet(self.title)
+
+        # number tracking periods
+        tp_size = len(project_object.tracking_periods)
+
+        # values for x_axis
+        if self.x_axis == XAxis.TRACKING_PERIOD:
+            names = ['Tracking Overview', 2, 0, (1+tp_size), 0]
+        elif self.x_axis == XAxis.DATE:
+            names = ['Tracking Overview', 2, 2, (1+tp_size), 2]
+
+        data_series = [
+            ["CV",
+             names,
+             ['Tracking Overview', 2, 9, (1+tp_size), 9]
+             ],
+        ]
+
+        labels = ["", ""]
+        chart = LineChart(self.title, labels, data_series)
+
+        size = {'width': 750, 'height': 500}
+        chart.draw(workbook, chartsheet, 'A1', None, size)
