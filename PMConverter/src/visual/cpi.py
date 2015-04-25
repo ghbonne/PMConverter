208ba0e2be6c7c7ff0dfe70c0fd5816b015469e2
ehoogsto <evelinekeh@gmail.com>
2015-04-25 23:41:11 +0200

    implemented threshold for cpi and spi(t)

--- PMConverter/src/visual/cpi.py
+++ PMConverter/src/visual/cpi.py
@@ -18,9 +18,9 @@ class CPI(Visualization):
         self.parameters = {"threshold": True,
                            "x-axis": [XAxis.TRACKING_PERIOD, XAxis.DATE]}
         self.x_axis = None
+        self.threshold = None
 
     def draw(self, workbook, worksheet, project_object): #todo probleem met percenten
-        #todo threshold!
         if not self.x_axis:
             raise Exception("Please first set var x_axis")
 
@@ -35,15 +35,47 @@ class CPI(Visualization):
         elif self.x_axis == XAxis.DATE:
             names = ['Tracking Overview', 2, 2, (1+tp_size), 2]
 
-        data_series = [
-            ["CPI",
-             names,
-             ['Tracking Overview', 2, 10, (1+tp_size), 10]
-             ],
-        ]
+        if self.threshold and self.threshold != (0, 0):
+            self.calculate_threshold(workbook, worksheet, tp_size)
+            data_series = [
+                ["CPI",
+                 names,
+                 ['Tracking Overview', 2, 10, (1+tp_size), 10]
+                 ],
+                ["threshold " + str(self.threshold),
+                  names,
+                  ['Tracking Overview', 2, 34, (1+tp_size), 34],
+                ]
+            ]
+        else:
+            data_series = [
+                ["CPI",
+                 names,
+                 ['Tracking Overview', 2, 10, (1+tp_size), 10]
+                 ],
+            ]
 
         labels = ["", ""]
         chart = LineChart(self.title, labels, data_series)
 
         size = {'width': 750, 'height': 500}
         chart.draw(workbook, chartsheet, 'A1', None, size)
+
+    def calculate_threshold(self, workbook, worksheet, tp_size):
+        header = workbook.add_format({'bold': True, 'bg_color': '#316AC5', 'font_color': 'white', 'text_wrap': True,
+                                      'border': 1, 'font_size': 8})
+        calculation = workbook.add_format({'bg_color': '#FFF2CC', 'text_wrap': True, 'border': 1, 'font_size': 8})
+
+        worksheet.write('AG2', 'CPI threshold', header)
+
+        start = 2
+        if self.threshold[0] == self.threshold[1]:
+            for i in range(0, tp_size):
+                worksheet.write(start + i, 34, self.threshold[0], calculation)
+        else:
+            if self.threshold[0] > self.threshold[1]:
+                value = (self.threshold[0] - self.threshold[1])/(tp_size - 1)
+            else:
+                value = (self.threshold[1] - self.threshold[0])/(tp_size - 1)
+            for i in range(0, tp_size):
+                worksheet.write(start + i, 34, self.threshold[0] + (i * value), calculation)
--- PMConverter/src/visual/spi_t.py
+++ PMConverter/src/visual/spi_t.py
@@ -20,7 +20,6 @@ class SpiT(Visualization):
         self.threshold = None
 
     def draw(self, workbook, worksheet, project_object):
-        #todo: threshold
         if not self.x_axis:
             raise Exception("Please first set var x_axis")
 
@@ -29,21 +28,32 @@ class SpiT(Visualization):
         # number tracking periods
         tp_size = len(project_object.tracking_periods)
 
-        if self.threshold and self.threshold != (0, 0):
-            self.calculate_threshold(workbook, worksheet, tp_size)
-
         # values for x_axis
         if self.x_axis == XAxis.TRACKING_PERIOD:
             names = ['Tracking Overview', 2, 0, (1+tp_size), 0]
         elif self.x_axis == XAxis.DATE:
             names = ['Tracking Overview', 2, 2, (1+tp_size), 2]
 
-        data_series = [
-            ["SPI(t)",
-             names,
-             ['Tracking Overview', 2, 12, (1+tp_size), 12]
-             ],
-        ]
+        if self.threshold and self.threshold != (0, 0):
+            self.calculate_threshold(workbook, worksheet, tp_size)
+            data_series = [
+                ["SPI(t)",
+                 names,
+                 ['Tracking Overview', 2, 12, (1+tp_size), 12]
+                 ],
+                ["threshold " + str(self.threshold),
+                  names,
+                  ['Tracking Overview', 2, 33, (1+tp_size), 33],
+                ]
+            ]
+        else:
+            data_series = [
+                ["SPI(t)",
+                 names,
+                 ['Tracking Overview', 2, 12, (1+tp_size), 12]
+                 ],
+            ]
+
 
         labels = ["", ""]
         chart = LineChart(self.title, labels, data_series)
@@ -56,16 +66,16 @@ class SpiT(Visualization):
                                       'border': 1, 'font_size': 8})
         calculation = workbook.add_format({'bg_color': '#FFF2CC', 'text_wrap': True, 'border': 1, 'font_size': 8})
 
-        worksheet.write('AD4', 'SPI(t) threshold', header)
+        worksheet.write('AH2', 'SPI(t) threshold', header)
 
         start = 2
         if self.threshold[0] == self.threshold[1]:
             for i in range(0, tp_size):
-                worksheet.write(start + i, 29, self.threshold[0], calculation)
+                worksheet.write(start + i, 33, self.threshold[0], calculation)
         else:
             if self.threshold[0] > self.threshold[1]:
-                value = (self.threshold[0] - self.threshold[1])/tp_size
+                value = (self.threshold[0] - self.threshold[1])/(tp_size - 1)
             else:
-                value = (self.threshold[1] - self.threshold[0])/tp_size
+                value = (self.threshold[1] - self.threshold[0])/(tp_size - 1)
             for i in range(0, tp_size):
-                worksheet.write(start + i, 29, value, calculation)
+                worksheet.write(start + i, 33, self.threshold[0] + (i * value), calculation)
