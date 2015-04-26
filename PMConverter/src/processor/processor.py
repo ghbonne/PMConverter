539c5409f77c646dea52f9761f27a27735da0f83
ehoogsto <evelinekeh@gmail.com>
2015-04-26 10:23:29 +0200

    draft processor convert method

--- PMConverter/src/processor/processor.py
+++ PMConverter/src/processor/processor.py
@@ -1,5 +1,5 @@
 __author__ = 'PM Group 8'
-
+import os
 from convert.XLSXparser import XLSXParser
 from convert.XMLparser import XMLParser
 from visual.charts.linechart import LineChart
@@ -28,13 +28,41 @@ class Processor(object):
                                "Tracking overview's visualisations", CostValueMetrics(), Performance(), SpiTvsPfactor(), SvT(), CV(), CPI(), SpiT()]
         self.file_parsers = []
 
-    def convert(self, parser_from, parser_to, file_path_from, file_path_to):
-        pass
+    def convert(self, parser_from, parser_to, file_path_from, visualisations=[], extended=0):
+        # Parse to project object
+        project_object = None
+        if parser_from == "protrack":
+            #todo: call Alexanders code
+            # save result in project object
+            pass
+        elif parser_from == "Excel":
+            xlsx_parser = XLSXParser()
+            project_object = xlsx_parser.to_schedule_object(os.path.join(os.path.dirname(__file__), file_path_from))
+
+        # Parse from project object
+        file_path_to = "" #todo: build file_path_to, based on file_path_from?
+        if parser_to == "Excel":
+            xlsx_parser = XLSXParser()
+            workbook = xlsx_parser.from_schedule_object(project_object, file_path_to)#todo: how do the parser know if it is basic or extended version?
+            if visualisations:
+                for worksheet in workbook.worksheets():
+                    if worksheet.get_name() == "Resources":
+                        pass
+                    if worksheet.get_name() == "Risk Analysis":
+                        pass
+                    if "TP" in worksheet.get_name():
+                        pass
+                    if worksheet.get_name() == "Baseline Schedule":
+                        pass
+                    if worksheet.get_name() == "Tracking Overview":
+                        pass
+                try:
+                    workbook.close()
+                except Exception:
+                    #todo: back to gui: "Can't write to file_path_to, please first close Excel and try again"
+                    pass
+                os.system("start excel.exe output/test2.xlsx")#todo: works only on windows, maybe not neccesary?
 
     def create_all_file_parsers(self):
         self.file_parsers.append(XLSXParser())
         self.file_parsers.append(XMLParser())
-
-    def create_all_visualizations(self):
-        self.visualizations.append(LineChart())
-        self.visualizations.append(PieChart())
