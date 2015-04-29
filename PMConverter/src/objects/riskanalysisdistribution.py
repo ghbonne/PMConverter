591ade33c2286f6a97811685bef63922cf7d73c6
ghbonne <gilles.bonne@ugent.be>
2015-04-29 18:09:10 +0200

    RiskAnalysisDistribution should not contain a distribution id nor ProTrack name.

--- PMConverter/src/objects/riskanalysisdistribution.py
+++ PMConverter/src/objects/riskanalysisdistribution.py
@@ -32,7 +32,7 @@ class RiskAnalysisDistribution(object):
     :var pessimistic_duration: int
     """
 
-    def __init__(self, distr_id=1, distr_name="Standard", distribution_type=DistributionType.MANUAL, distribution_units=ManualDistributionUnit.ABSOLUTE, optimistic_duration=0,
+    def __init__(self, distribution_type=DistributionType.MANUAL, distribution_units=ManualDistributionUnit.ABSOLUTE, optimistic_duration=0,
                  probable_duration=0, pessimistic_duration=0, type_check=True):
         if type_check:
             if type(distribution_type) is str:
@@ -41,7 +41,7 @@ class RiskAnalysisDistribution(object):
                 elif distribution_type.lower() == "standard":
                     distribution_type = DistributionType.STANDARD
             if not isinstance(distribution_type, DistributionType):
-                raise TypeError('distribution_type should be an element of the DistributionType enum')
+                raise TypeError('RiskAnalysisDistribution: distribution_type should be an element of the DistributionType enum')
             if distribution_type == DistributionType.MANUAL:
                 if type(distribution_units) is str:
                     if distribution_units.lower() == "relative":
@@ -49,7 +49,7 @@ class RiskAnalysisDistribution(object):
                     elif distribution_units.lower() == "absolute":
                         distribution_units = ManualDistributionUnit.ABSOLUTE
                     if not isinstance(distribution_units, ManualDistributionUnit):
-                        raise TypeError('Since distribution_type = MANUAL, distribution_units should be an element of '
+                        raise TypeError('RiskAnalysisDistribution: Since distribution_type = MANUAL, distribution_units should be an element of '
                                         'ManualDistributionUnit enum')
             if distribution_type == DistributionType.STANDARD:
                 if type(distribution_units) is str:
@@ -62,15 +62,13 @@ class RiskAnalysisDistribution(object):
                     elif distribution_units.lower() == "skewed right":
                         distribution_units = StandardDistributionUnit.SKEWED_RIGHT
                     if not isinstance(distribution_units, StandardDistributionUnit):
-                        raise TypeError('Since distribution_type = STANDARD, distribution_units should be an element of StandardDistributionUnit enum')
+                        raise TypeError('RiskAnalysisDistribution: Since distribution_type = STANDARD, distribution_units should be an element of StandardDistributionUnit enum')
             if not isinstance(optimistic_duration, int):
-                raise TypeError('optimistic_duration should be an integer')
+                raise TypeError('RiskAnalysisDistribution: optimistic_duration should be an integer')
             if not isinstance(probable_duration, int):
-                raise TypeError('probable_duration should be an integer')
+                raise TypeError('RiskAnalysisDistribution: probable_duration should be an integer')
             if not isinstance(pessimistic_duration, int):
-                raise TypeError('pessimistic_duration should be an integer')
-        self.distr_id = distr_id
-        self.distr_name = distr_name
+                raise TypeError('RiskAnalysisDistribution: pessimistic_duration should be an integer')
         self.distribution_type = distribution_type
         self.distribution_units = distribution_units
         self.optimistic_duration = optimistic_duration
