5b6b0f752f93df1db1f0a8fcb22d479c70abb335
ghbonne <gilles.bonne@ugent.be>
2015-05-15 19:53:26 +0200

    changed pmconverter traceback print

--- PMConverter/src/pmconverter.py
+++ PMConverter/src/pmconverter.py
@@ -1,6 +1,7 @@
 from PyQt4.QtGui import *
 from view.uiview import UIView
 import sys
+import traceback
     
 try:
     app = QApplication(sys.argv)
@@ -11,5 +12,5 @@ try:
 except:
     print("Unhandled Exception occurred in PMConverter of type: {0}\n".format(sys.exc_info()[0]))
     print("Unhandled Exception value = {0}\n".format(sys.exc_info()[1] if sys.exc_info()[1] is not None else "None"))
-    print("Unhandled Exception traceback = {0}\n".format(sys.exc_info()[2] if sys.exc_info()[2] is not None else "None"))
+    print("Unhandled Exception traceback = {0}\n".format(traceback.format_exc()))
     print("\PMConverter CRASHED\n")
